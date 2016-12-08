7e838dbff226145958a72d336de4af33e78900e9
Julien Barbier <write0@gmail.com>
2016-12-07 23:11:53 -0800

    Update check_simple_shell.bash

--- check_simple_shell.bash
+++ check_simple_shell.bash
@@ -1,28 +1,63 @@
 #!/bin/bash
+#
+# Performing checks on the Holberton School "simple shell" project
 
+#######################################
+# Print KO, in red, followed by a new line
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
 function print_ko()
 {
     echo -e "[\033[31mKO\033[37m]"
 }
 
+#######################################
+# Print OK in green, followed by a new line
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
 function print_ok()
 {
     echo -e "[\033[32mOK\033[37m]"
 }
 
+#######################################
+# Kill the shell in a clean way and remove temporary files
+# Globals:
+#   SHELL
+#   OUTPUTFILE
+#   ERROROUTPUTFILE
+#   LTRACEOUTPUTFILE
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
 function stop_shell()
 {
     if [ `pidof $SHELL | wc -l` -ne 0 ]; then
 	   killall -9 $SHELL 2>&1 > /dev/null
     fi
-    rm -f $PIPE $OUTPUTFILE $ERROROUTPUTFILE $LTRACEOUTPUTFILE
+    rm -f $OUTPUTFILE $ERROROUTPUTFILE $LTRACEOUTPUTFILE
 }
 
+# Load configuration
 source config
 
+# Cleanup
 echo -ne "\033[37m"
 rm -f $OUTPUTFILE $LTRACEOUTPUTFILE
 
+# Locates all tests and launch them
 for dir in `ls -d "$TESTDIR"/*/`
 do
     echo "> $dir"
@@ -33,6 +68,7 @@ do
     done
 done
 
+# Cleanup
 rm -f $OUTPUTFILE $LTRACEOUTPUTFILE $ERROROUTPUTFILE
 rm -f checker_output_*
 rm -f checker_tmp_file_*
