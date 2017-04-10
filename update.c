157e76394bc25eb406fab4fae09e8de731f933ce
glyif <122@holbertonschool.com>
2017-04-09 18:42:07 -0700

    updated with dir removal

--- update.c
+++ update.c
@@ -17,7 +17,7 @@ void update_shellfish(void)
 {
 	int check;
 	
-	check = system("git clone https://github.com/glyif/shellfish.git && cd shellfish && sudo ./install.sh");
+	check = system("git clone https://github.com/glyif/shellfish.git && cd shellfish && sudo ./install.sh && cd ../ && rm -rf shellfish");
 
 	if (check == -1)
 		perror("You done messed up");
