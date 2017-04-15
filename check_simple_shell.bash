d10d7e84fcd188631d02cb32d0fbddef7a0f016a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-15 16:16:16 +0000

    updated colors, made function for print-error, made function for check diff

--- check_simple_shell.bash
+++ check_simple_shell.bash
@@ -31,11 +31,68 @@ function print_ok()
 }
 
 #######################################
+# Prints error messages if any
+# Globals:
+#   RED
+#   ERROROUTPUTFILE
+#   ERROREXPECTED
+#   BLACK
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+function print_error()
+{
+	echo -ne "\033[31m"
+	if [ -s $ERROROUTPUTFILE ]; then
+		echo ""
+		echo "your error: " && cat $ERROROUTPUTFILE
+		echo ""
+	fi
+	if [ -s $ERROREXPECTED ]; then
+		echo ""
+		echo "expected error: " && cat $ERROREXPECTED
+		echo ""
+	fi
+	echo -ne "\033[37m"
+}
+
+#######################################
+# Checks differences
+# Globals:
+#   DIFF
+#   OUTPUTFILE
+#   EXPECTED
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+function check_diff()
+{
+	diff $OUTPUTFILE $EXPECTED > $DIFF
+	if [ -s $DIFF ]; then
+		print_ko
+		echo -ne "\033[30m"
+		echo "this is the difference between your output and the expected:"
+		echo -ne "\033[31m"
+		cat $DIFF
+		echo -ne "\033[37m"
+	else
+		print_ok
+	fi
+}
+
+#######################################
 # Kill the shell in a clean way and remove temporary files
 # Globals:
 #   SHELL
 #   OUTPUTFILE
+#   EXPECTED
+#   DIFF
 #   ERROROUTPUTFILE
+#   ERROREXPECTED
 #   LTRACEOUTPUTFILE
 # Arguments:
 #   None
@@ -47,7 +104,7 @@ function stop_shell()
     if [ `pidof $SHELL | wc -l` -ne 0 ]; then
 	   killall -9 $SHELL 2>&1 > /dev/null
     fi
-	> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE
+	> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE && > $ERROREXPECTED
 	rm -f $LTRACEOUTPUTFILE
 	rm -f checker_tmp_file_*
 }
@@ -56,7 +113,7 @@ function stop_shell()
 source config
 
 # Introduction
-echo -ne "\033[30m"
+echo -ne "\033[34m"
 echo "     *************************************"
 echo "     ***                               ***"
 echo "     ***     Beginning Test Suite      ***"
@@ -70,6 +127,7 @@ cat AUTHORS | tail -n +3
 echo ""
 
 # Prompt to check to continue
+echo -ne "\033[31m"
 echo "type 'y' or 'Y' & click enter to continue, or"
 read -p "type anything else to quit: " choice
 case "$choice" in
@@ -79,7 +137,7 @@ esac
 
 # Cleanup
 echo -ne "\033[37m"
-> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE
+> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE && > $ERROREXPECTED
 rm -f $LTRACEOUTPUTFILE
 rm -f checker_tmp_file_*
 
@@ -95,8 +153,9 @@ do
 done
 
 # Cleanup
-> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE
+> $OUTPUTFILE && > $EXPECTED && > $DIFF && > $ERROROUTPUTFILE && > $ERROREXPECTED
 rm -f $LTRACEOUTPUTFILE
 rm -f checker_tmp_file_*
 rm -f /tmp/.checker_tmp_file_*
+rm -f > $OUTPUTFILE $EXPECTED $DIFF $ERROROUTPUTFILE $ERROREXPECTED
 echo -ne "\033[30m"
